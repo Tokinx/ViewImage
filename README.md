15331825d995b9c1d29d1c38c54a1874c1d28d27
Cheng <742206125@qq.com>
2017-02-13 18:13:53 +0800

    v1.0.0
    1kb minimalist lightbox.

---
+++ README.md
@@ -0,0 +1,66 @@
+# ViewImage.js
+
+基于jQuery ，Gzip后不足 1kb 的极简灯箱插件
+
+## 获取ViewImage.js
+
+你可以直接访问 ViewImage for GitHub 来获取最新版插件
+
+当然你也可以通过后面的链接下载ViewImage：view-image.js or view-image.min.js
+
+## 如何使用
+
+ViewImage是一个基于jQuery的插件，首先确保你的页面已经正确引用该库类，然后再引用ViewImage.js。
+
+    <script src="//view-image.git.nz/static/view-image.min.js"></script>
+
+## 初始化并启用
+
+我们为您提供了非常简便的初始化方法，方便您对程序进行一些个性化设置并正确启用
+
+    <script>
+        jQuery(document).ready(function () {
+            jQuery.viewImage({
+                'target' : '.view-image img', //需要使用ViewImage的图片
+                'exclude': '.exclude img',    //要排除的图片
+                'delay'  : 300                //延迟时间
+            });
+        });
+    </script>
+
+## DEMO
+
+我们提供了几个DEMO，方便您进一步了解ViewImage
+
+    <script>
+        jQuery(document).ready(function () {
+            jQuery.viewImage({
+                'target': '.view-image img,.view-image2 a,.view-image3 a',
+                'exclude': '.exc',
+                'delay': 300
+            });
+        });
+    </script>
+
+### 建议使用方法
+
+    <ul class="view-image">
+        <li><img src="原图"></li>
+        <li><img src="原图"></li>
+        <li><img src="原图"></li>
+        <li><img src="原图" class="exc"></li>
+    </ul>
+
+### A 链接指向图片
+
+    <ul class="view-image2">
+        <li><a href="原图">可用</a></li>
+        <li><a href="原图" class="exc">不可用</a></li>
+    </ul>
+
+### A 链接指向图片并嵌套图片
+
+    <ul class="view-image3">
+    <li><a href="原图"><img src="缩略图 或 原图"></a></li>
+    <li><a href="原图" class="exc"><img src="缩略图 或 原图"></a></li>
+    </ul>
---
+++ view-image.js
@@ -0,0 +1,51 @@
+/**
+ * ViewImage is a jQuery plugin that makes it easy to support automatically
+ * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
+ *
+ * @name ViewImage.js
+ * @version 1.0.0
+ * @requires jQuery v2.0+
+ * @author Tokin (Tokinx)
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ *
+ * For usage and examples, visit:
+ * http://view-image.git.nz/
+ *
+ * Copyright (c) 2017, Biji.IO
+ */
+(function ($) {
+    $.extend({
+        viewImage: function (options) {
+            var setting = $.extend({
+                'target': '.view-image img',
+                'exclude': '',
+                'delay': 300
+            }, options);
+            $(setting.exclude).attr("view-image", false);
+            $(setting.target).click(function () {
+                var src = $(this).attr('src'),
+                    href = $(this).attr('href'),
+                    url = '',
+                    vicss = "<style class='view-image-css'>.view-img{position:fixed;background:#fff;background:rgba(255,255,255,.99);width:100%;height:100%;top:0;left:0;text-align:center;padding:2%;z-index:999;cursor: zoom-out}.view-img img,.view-img span{max-width:100%;max-height:100%;position:relative;top:50%;transform: translateY(-50%);}.view-img img{animation:view-img-show .8s -0.1s ease-in-out}.view-img span{height:2em;color:#AAB2BD;overflow:hidden;position:absolute;top:50%;left:0;right:0;width:120px;text-align:center;margin:-1em auto;}.view-img span:after{content:'';position:absolute;bottom:0;left:0;transform: translateX(-100%);width:100%;height:2px;background:#3274ff;animation:view-img-load .8s -0.1s ease-in-out infinite;}@keyframes view-img-load{0%{transform: translateX(-100%);}100%{transform: translateX(100%);}}@keyframes view-img-show{0%{opacity:0;}100%{opacity:1;}}</style>";
+                if (!$(this).attr("view-image") && !$(this).attr("rel")) {
+                    url = src ? src : href;
+                    $("body").append(vicss + "<div class='view-img'><span>loading...</span></div>");
+                    setTimeout(function () {
+                        var obj = new Image();
+                        obj.src = url;
+                        obj.onload = function () {
+                            //console.log("Width:" + obj.width + " Height:" + obj.height);
+                            $(".view-img").html("<img src=" + this.src + ">");
+                        }
+                        $('.view-img').click(function () {
+                            $('.view-image-css').remove();
+                            $(this).remove();
+                        });
+                    }, setting.delay);
+                    return false;
+                }
+            });
+            return;
+        }
+    });
+})(jQuery);
