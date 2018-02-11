75f2938a49adaecafbe7a8f5e70b783893d73dd0
Cheng <idevs@foxmail.com>
2018-02-11 13:34:54 +0800

    v1.2.0

--- view-image.js
+++ view-image.js
@@ -28,7 +28,8 @@
                     ww: $(window).width(),
                     wh: $(window).height()
                 };
-                if (!$(this).attr("view-image") && !$(this).is(setting.exclude) && !$(this).attr("rel") && (dt.t.attr('src') || dt.t.attr('href').match(/.+\.(jpg|jpeg|webp|gif|png)/gi))) {
+                console.log(dt.t)
+                if (!$(this).attr("view-image") && !$(this).is(setting.exclude) && (dt.t.attr('src') || dt.t.attr('href').match(/.+\.(jpg|jpeg|webp|gif|png)/gi))) {
                     if (dt.t.attr('src')) {
                         dt.is = (dt.t[0].style.cursor === "zoom-out");
                         dt.os = dt.t.offset();
@@ -39,14 +40,9 @@
                         dt.Y = dt.is ? 0 : (-dt.os.top + (dt.wh - dt.h) / 2 + $(document).scrollTop()) / dt.scale;
                         dt.t.attr('style', (dt.is ? "" : "position: relative;z-index: 999;") + "transition: transform 0.4s;transform: scale(" + dt.scale + ") translate(" + dt.X + "px, " + dt.Y + "px);cursor: zoom-" + (dt.is ? "in" : "out") + ";");
                     } else {
-                        dt.w = dt.m(dt.o.width, dt.ww * dt.z);
-                        dt.h = dt.m(dt.o.height, dt.wh * dt.z);
-                        dt.o = new Image().src = dt.t.attr('href');
                         dt.c = ".vi-" + Math.random().toString(36).substr(2);
                         $("body").append("<div class='" + dt.c.substr(1) + "' style='position: fixed;background: rgba(255, 255, 255, " + dt.z + ");top: 0;left: 0;right: 0;bottom: 0;z-index: 999;'></div>");
-                        dt.o.onload = function () {
-                            $(dt.c).html("<img src=" + this.src + " style='position: absolute;top: 50%;left: 50%;margin-top:-" + (dt.h / 2) + "px;margin-left:-" + (dt.w / 2) + "px;'>");
-                        }
+                        $(dt.c).html("<img src=" + dt.t.attr('href') + " style='position: absolute;top: 50%;left: 50%;max-width: 90%;max-height: 90%;transform: translate(-50%,-50%);'>");
                         $(dt.c).click(function () {
                             $(this).remove();
                         });
