87c50507afe2e0a39a23bf71a8ec21014e23585f
Cheng <idevs@foxmail.com>
2020-08-05 17:42:57 +0800

    Update view-image.js
    fix bugs

--- view-image.js
+++ view-image.js
@@ -23,8 +23,8 @@
             $(setting.exclude).attr("view-image", false);
             $(setting.target).off().on('click', function (el) {
                 let dt = {
-                    src: el.target.src,
-                    href: el.target.href,
+                    src: el.currentTarget.src,
+                    href: el.currentTarget.href,
                     style: "<style class='view-image-css'>.view-img{position:fixed;background:#fff;background:rgba(255,255,255,.92);width:100%;height:100%;top:0;left:0;text-align:center;padding:2%;z-index:999;cursor: zoom-out}.view-img img,.view-img span{max-width:100%;max-height:100%;position:relative;top:50%;transform: translateY(-50%);}.view-img img{animation:view-img-show .8s -0.1s ease-in-out}.view-img span{height:2em;color:#AAB2BD;overflow:hidden;position:absolute;top:50%;left:0;right:0;width:120px;text-align:center;margin:-1em auto;}.view-img span:after{content:'';position:absolute;bottom:0;left:0;transform: translateX(-100%);width:100%;height:2px;background:#3274ff;animation:view-img-load .8s -0.1s ease-in-out infinite;}@keyframes view-img-load{0%{transform: translateX(-100%);}100%{transform: translateX(100%);}}@keyframes view-img-show{0%{opacity:0;}100%{opacity:1;}}</style>",
                     class: ".vi-" + Math.random().toString(36).substr(2)
                 };
